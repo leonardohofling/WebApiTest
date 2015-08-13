44c919a968f04d3a49952c65a1d99e5f45049ff8
Leonardo Hofling <lmhofling@gmail.com>
2015-08-13 10:05:06 -0300

    added firt version

---
+++ WebApiTest/App_Start/Startup.Auth.cs
@@ -0,0 +1,31 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+using WebApiTest.Providers;
+
+namespace WebApiTest
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AllowInsecureHttp = true
+            });
+        }
+    }
+}
---
+++ WebApiTest/Providers/ApplicationOAuthProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Providers
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext c)
+        {
+            c.Validated();
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext c)
+        {
+            if (c.UserName == "leonardo" && c.Password == "123123")
+            {
+                Claim claim1 = new Claim(ClaimTypes.Name, c.UserName);
+                Claim[] claims = new Claim[] { claim1 };
+                ClaimsIdentity claimsIdentity =
+                    new ClaimsIdentity(
+                       claims, OAuthDefaults.AuthenticationType);
+                c.Validated(claimsIdentity);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
