b86d6fa4e1dd9186ba87e75269b4e6e526044898
Leonardo Hofling <lmhofling@gmail.com>
2015-08-13 11:07:40 -0300

    Added MeController

---
+++ WebApiTest/Controllers/MeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiTest.Controllers
+{
+    public class MeController : ApiController
+    {
+        private IAuthenticationManager Authentication
+        {
+            get { return HttpContext.Current.GetOwinContext().Authentication; }
+        }
+
+        // GET api/values
+        [Authorize]
+        public string Get()
+        {
+            return this.Authentication.User.Identity.Name;
+        }
+    }
+
+
+}
